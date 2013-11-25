dfc2b7e27d9f23a87b2d3782e20a6b64535eade8
Kane Cohen <kanecohen@gmail.com>
2013-11-25 22:45:15 +0000

    Bring back fixed getSize. Forgot that it's needed.

--- src/Cohensive/Validation/Validator.php
+++ src/Cohensive/Validation/Validator.php
@@ -103,6 +103,39 @@ class Validator extends BaseValidator implements MessageProviderInterface
 		}
 	}
 
+	/*
+	 * Get the size of an attribute.
+	 *
+	 * @param  string  $attribute
+	 * @param  mixed   $value
+	 * @return mixed
+	 */
+	protected function getSize($attribute, $value)
+	{
+		$hasNumeric = $this->hasRule($attribute, $this->numericRules);
+
+		// This method will determine if the attribute is a number, string, or file and
+		// return the proper size accordingly. If it is a number, then number itself
+		// is the size. If it is a file, we take kilobytes, and for a string the
+		// entire length of the string will be considered the attribute size.
+		if (is_numeric($value) and $hasNumeric)
+		{
+			return $value;
+		}
+		elseif (is_array($value))
+		{
+			return count($value);
+		}
+		elseif ($value instanceof File)
+		{
+			return $value->getSize() / 1024;
+		}
+		else
+		{
+			return $this->getStringSize($value);
+		}
+	}
+
 	/**
 	 * Validate that a required attribute exists.
 	 *
