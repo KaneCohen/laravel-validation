df3e84694dc485f8cf0fec1f07641cf726a3dece
Kane Cohen <kanecohen@gmail.com>
2013-12-11 22:12:16 +0000

    Fix factory.

--- src/Cohensive/Validation/Factory.php
+++ src/Cohensive/Validation/Factory.php
@@ -19,14 +19,14 @@ class Factory extends BaseFactory
 	 * @param  array  $data
 	 * @param  array  $rules
 	 * @param  array  $messages
-	 * @return \Cohenisve\Validation\Validator
+	 * @return \Illuminate\Validation\Validator
 	 */
-	public function make(array $data, array $rules, array $messages = array())
+	public function make(array $data, array $rules, array $messages = array(), array $customAttributes = array())
 	{
 		// The presence verifier is responsible for checking the unique and exists data
 		// for the validator. It is behind an interface so that multiple versions of
 		// it may be written besides database. We'll inject it into the validator.
-		$validator = $this->resolve($data, $rules, $messages);
+		$validator = $this->resolve($data, $rules, $messages, $customAttributes);
 
 		if ( ! is_null($this->verifier))
 		{
@@ -34,22 +34,15 @@ class Factory extends BaseFactory
 		}
 
 		// Next we'll set the IoC container instance of the validator, which is used to
-		// resolves out class baesd validator extensions. If it's not set then these
+		// resolve out class based validator extensions. If it is not set then these
 		// types of extensions will not be possible on these validation instances.
 		if ( ! is_null($this->container))
 		{
 			$validator->setContainer($this->container);
 		}
 
-		$validator->addExtensions($this->extensions);
+		$this->addExtensions($validator);
 		$validator->addReplacements($this->replacements);
-
-		// Next, we will add the implicit extensions, which are similar to the required
-		// and accepted rule in that they are run even if the attributes is not in a
-		// array of data that is given to a validator instances via instantiation.
-		$implicit = $this->implicitExtensions;
-
-		$validator->addImplicitExtensions($implicit);
 
 		return $validator;
 	}
@@ -60,7 +53,7 @@ class Factory extends BaseFactory
 	 * @param  array  $data
 	 * @param  array  $rules
 	 * @param  array  $messages
-	 * @return \Cohensive\Validation\Validator
+	 * @return \Illuminate\Validation\Validator
 	 */
 	protected function resolve($data, $rules, $messages, $customAttributes)
 	{
